25c519b4140da608c93ce7fe2f8d05ed33b7575b
DylanMC14 <dmadrigalc@fwdcostarica.com>
2023-09-05 16:01:41 -0600

    avance del pokedex 2da parte

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -1,10 +1,70 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import "../Styles/Pokedex.css";
+import {getPokemonList} from "./PokePrevNext";
+import defaultImage from '../Assets/Img/gif3.gif';
 
- function Pokedex() {
+
+  function Pokedex(){
+
+    // randomPokemons almacenar los Pokemon
+    const [isList, setIsList ] = useState ([]);
+    useEffect ( () =>{
+        const fetchPokemon = async () => {
+            try {
+              
+              var response = await getPokemonList('https://pokeapi.co/api/v2/pokemon?limit=20');
+              var data = response.array;
+              setIsList(data);
+              console.log(data);
+              
+              
+              
+            } catch (error) {
+              console.error("Error capturando Pokemon data", error);
+            }
+            
+          };
+          fetchPokemon();
+        }, []);
+        const handleImageLoad = (event) => {
+          event.target.src = event.target.dataset.src;
+        };
+        
+
+
     return ( 
         <div className='boxPokedex'>
-        <h1>Pokedex</h1>
+        <div className="pokeBox">
+        {isList.map((pokemon) => (
+          <div id='poke1' key={pokemon.id}>
+            
+            <img className='poke-Card'
+            src={defaultImage}
+            alt={pokemon.name}
+            data-src={pokemon.image} // Almacenar la URL real en un atributo personalizado
+            onLoad={handleImageLoad} // Manejador de carga de imagen
+            loading="lazy" // Agregar atributo "loading" con valor "lazy"
+            style={{maxHeight:'100px', maxWidth: "100px", borderRadius:"5px",}}/>
+            <div className='poke-Card-info'>
+              <p>
+                {pokemon.name}
+              </p>
+              <p>
+                Height: {pokemon.height}
+              </p>
+              <p>
+                Weight: {pokemon.weight}
+              </p>
+              <p>
+                {/* Types: {pokemon.types} falta ver como recorrer eel array */}
+              </p>
+              <p>
+                Stats: {pokemon.stats}
+              </p>
+            </div>
+          </div>
+        ))}
+      </div>
       </div>
      );
  }

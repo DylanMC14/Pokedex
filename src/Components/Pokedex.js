4a03828e650b2fa392af04a2dbdd982db9745efc
DylanMC14 <dmadrigalc@fwdcostarica.com>
2023-09-14 15:53:12 -0600

    commit despues de la presentacion 14/09/2023

--- src/Components/Pokedex.js
+++ src/Components/Pokedex.js
@@ -9,9 +9,9 @@ import VanillaTilt from "vanilla-tilt";
 
 function Pokedex() {
   //update cards
-  const [updateCards, setUpdateCards] = useState(false);
+  const [updateCards,setUpdateCards]=useState(false)
   //favoritos
-  const [favorites, setFavorites] = useState([]);
+  const [favorites,setFavorites]=useState([])
   // randomPokemons almacenar los Pokemon
   const [isList, setIsList] = useState([]);
   const [filteredPokemon, setFilteredPokemon] = useState([]);
@@ -30,8 +30,8 @@ function Pokedex() {
         var data = response.array;
         setIsList(data);
         //pokemones favoritos
-        const dataFavorites = await getPokemonMockApi();
-        setFavorites(dataFavorites);
+        const dataFavorites=await getPokemonMockApi();
+        setFavorites(dataFavorites)
       } catch (error) {
         console.error("Error capturando Pokemon data", error);
       }
@@ -39,8 +39,8 @@ function Pokedex() {
     fetchPokemon();
   }, [updateCards]);
 
-  useEffect(() => {
-    VanillaTilt.init(document.querySelectorAll(".poke1"), {
+  useEffect(()=>{
+    VanillaTilt.init(document.querySelectorAll(".poke1"),{
       scale: 1.1,
       perspective: 2000,
       easing: "cubic-bezier(.05,.100,.55,.104)",
@@ -48,18 +48,18 @@ function Pokedex() {
       maxTilt: 20,
       reset: true,
       speed: 300,
-    });
-    VanillaTilt.init(document.querySelectorAll(".info"), {
+    })
+    VanillaTilt.init(document.querySelectorAll(".info"),{
       reset: true,
       perspective: 2000,
       speed: 300,
-    });
-    VanillaTilt.init(document.querySelectorAll(".poke-Card"), {
+    })
+    VanillaTilt.init(document.querySelectorAll(".poke-Card"),{
       reset: true,
       perspective: 2000,
       speed: 300,
-    });
-  });
+    })
+  })
   const handleImageLoad = (event) => {
     event.target.src = event.target.dataset.src;
   };
@@ -68,11 +68,12 @@ function Pokedex() {
     console.log(filteredData);
     setFilteredPokemon(filteredData);
   };
-  function update() {
-    setUpdateCards(!updateCards);
+  function update(){
+    setUpdateCards(!updateCards)
   }
 
   return (
+
     <div className="boxPokedex">
       <div className="pokedex-title">
         <h1>Pokedex</h1>
@@ -82,54 +83,49 @@ function Pokedex() {
       </div>
       <div id="text">
         <h5>
-          The ⭐ means that the pokemon is added to favorites <br />
-          The ❌ would be to remove the pokemon from the favorites list
+        The ⭐ means that the pokemon is added to favorites <br/>
+        The ❌ would be to remove the pokemon from the favorites list
         </h5>
       </div>
       <div className="pokeBox">
         {(filteredPokemon.length > 0 ? filteredPokemon : isList)
           .map((pokemon) => (
-            <div>
+            <div >
               <div className="poke1 rg" key={pokemon.id}>
-                <div className="poke-Card">
-                  <img
-                    src={defaultImage}
-                    alt={pokemon.name}
-                    data-src={pokemon.image2} // Almacenar la URL real en un atributo personalizado
-                    onLoad={handleImageLoad} // Manejador de carga de imagen
-                    loading="lazy" // Agregar atributo "loading" con valor "lazy"
-                    style={{
-                      maxHeight: "100px",
-                      maxWidth: "100px",
-                      borderRadius: "5px",
-                    }}
-                  />
+              <div className="poke-Card">
+                <img
+                  src={defaultImage}
+                  alt={pokemon.name}
+                  data-src={pokemon.image} // Almacenar la URL real en un atributo personalizado
+                  onLoad={handleImageLoad} // Manejador de carga de imagen
+                  loading="lazy" // Agregar atributo "loading" con valor "lazy"
+                  style={{
+                    maxHeight: "100px",
+                    maxWidth: "100px",
+                    borderRadius: "5px",
+                  }}
+                />
+              </div>
+              <div className="poke-Card-info">
+                <div className="info">
+                <p>
+                  {pokemon.name} <br/>
+                  # {pokemon.id}
+                </p>
+                <p>Types: {pokemon.types}</p>
+              <p>
+              Stats: <br/> Hp: {pokemon.hp} / Attack: {pokemon.attack} <br/> 
+                Defense: {pokemon.defense} / Speed: {pokemon.speed}
+              </p>
+                <p>Height: {pokemon.height} <br/> Weight: {pokemon.weight}</p>
                 </div>
-                <div className="poke-Card-info">
-                  <div className="info">
-                    <p>
-                      {pokemon.name} <br /># {pokemon.id}
-                    </p>
-                    <p>Types: {pokemon.types}</p>
-                    <p>
-                      Stats: <br /> Hp: {pokemon.hp} / Attack: {pokemon.attack}{" "}
-                      <br />
-                      Defense: {pokemon.defense} / Speed: {pokemon.speed}
-                    </p>
-                    <p>
-                      Height: {pokemon.height} <br /> Weight: {pokemon.weight}
-                    </p>
-                  </div>
-                  <div className="poke-Btn-Fav-Del">
-                    <FavoritePokemon
-                      name={pokemon.name}
-                      favorites={favorites}
-                      update={update}
-                    />
-                  </div>
+                <div className="poke-Btn-Fav-Del">
+                  <FavoritePokemon name={pokemon.name} favorites={favorites} update={update} />
                 </div>
               </div>
             </div>
+            </div>
+            
           ))
           .slice(firstPosition, laststPosition)}
       </div>
